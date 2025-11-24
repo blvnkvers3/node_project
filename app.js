bb4d03068b38464a932f43672eecdd114b8354b6
blvnkvers3 <georgechi0@hotmail.com>
2025-11-24 15:31:00 -0700

    chris_fix

--- app.js
+++ app.js
@@ -1,9 +1,10 @@
+
 const express = require('express');
 const app = express();
 const port = 3000;
 
 app.get('/', (req, res) => {
-  res.send('Hello, Docker! Water Bottle');
+  res.send('Hello, Docker! Printer');
 });
 
 app.listen(port, () => {
